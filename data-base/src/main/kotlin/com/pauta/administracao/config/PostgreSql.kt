a6b69d58a1c7c741d69d6aec06ed193d6517c0cd
seyfero <saulocarvalho64@gmail.com>
2023-02-13 19:37:24 -0300

    Configuração do banco

---
+++ data-base/src/main/kotlin/com/pauta/administracao/config/PostgreSql.kt
@@ -0,0 +1,40 @@
+package com.pauta.administracao.config
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
+import io.r2dbc.postgresql.PostgresqlConnectionFactory
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class PostgreSql(
+    @Value("\${spring.r2dbc.host}")
+    private val host: String,
+
+    @Value("\${spring.r2dbc.port}")
+    private val port: String,
+
+    @Value("\${spring.r2dbc.data-base}")
+    private val dataBase: String,
+
+    @Value("\${spring.r2dbc.username}")
+    private val username: String,
+
+    @Value("\${spring.r2dbc.password}")
+    private val password: String
+) {
+
+    @Bean
+    fun connectionFactory(): ConnectionFactory {
+        return PostgresqlConnectionFactory(
+            PostgresqlConnectionConfiguration.builder()
+            .host(host)
+            .port(port.toInt())
+            .database(dataBase)
+            .username(username)
+            .password(password)
+            .build())
+    }
+
+}
