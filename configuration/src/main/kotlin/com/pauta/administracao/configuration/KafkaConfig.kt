b76c984323c73002a28fb9a000ddc2ea5cf26f0f
seyfero <saulocarvalho64@gmail.com>
2023-02-20 19:49:10 -0300

    Criação do tópico se não existe

--- configuration/src/main/kotlin/com/pauta/administracao/configuration/KafkaConfig.kt
+++ configuration/src/main/kotlin/com/pauta/administracao/configuration/KafkaConfig.kt
@@ -1,6 +1,9 @@
 package com.pauta.administracao.configuration
 
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,6 +11,7 @@ import org.springframework.core.serializer.Serializer
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import reactor.core.publisher.Mono
 
 @Configuration
 class KafkaConfig {
@@ -23,6 +27,8 @@ class KafkaConfig {
     @Value("\${kafka.producer.acks}")
     private val acks: String? = null
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Bean
     fun producerFactory(): ProducerFactory<String, String> {
         val configs: MutableMap<String, Any> = HashMap()
@@ -35,7 +41,32 @@ class KafkaConfig {
     }
 
     @Bean
+    fun adminClient(): AdminClient {
+        val props = HashMap<String, Any?>()
+        props["bootstrap.servers"] = bootstrapServers
+        return AdminClient.create(props)
+    }
+
+    @Bean
     fun kafkaTemplate(): KafkaTemplate<String, String> {
         return KafkaTemplate(producerFactory())
+    }
+
+    @Bean
+    fun createTopicIfNotExists(adminClient: AdminClient,
+                               @Value("\${kafka.topic.name}") topicName: String,
+                               @Value("\${kafka.topic.partition}") numPartitions: Int,
+                               @Value("\${kafka.topic.replicationFactor}") replicationFactor: Short,
+    ): NewTopic {
+        val existingTopics = adminClient.listTopics().names().get()
+        return if (!existingTopics.contains(topicName)) {
+            val newTopic = NewTopic(topicName, numPartitions, replicationFactor)
+            adminClient.createTopics(listOf(newTopic)).all().get()
+            logger.info("Topic created with success!")
+            newTopic
+        } else {
+            logger.info("Topic exist with this name!")
+            NewTopic(topicName, numPartitions, replicationFactor)
+        }
     }
 }
